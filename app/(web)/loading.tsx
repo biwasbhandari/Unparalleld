6b4bde1b261b0ed24ed87366a511078cadf736af
Asish Bhandari <125468428+cynic-ace@users.noreply.github.com>
2024-01-16 20:00:55 +0545

    schema updated with featured product

--- app/(web)/loading.tsx
+++ app/(web)/loading.tsx
@@ -1,8 +1,8 @@
 const LoadinSpinner = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-200">
-        <h1>Loading</h1>
+      <div className=" rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-200">
+        <h1>Loading...</h1>
       </div>
     </div>
   );
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "images.unsplash.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "images.unsplash.com",
+      "unsplash.com",
+    ],
   },
 };
 
