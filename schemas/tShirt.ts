a19c79a85149ba1dfe08f20c7be4fb1780eb71b9
Asish Bhandari <125468428+cynic-ace@users.noreply.github.com>
2024-01-19 13:51:24 +0545

    almost done

---
+++ schemas/tShirt.ts
@@ -0,0 +1,115 @@
+import { defineField } from "sanity";
+
+const shirtSize = [
+  { title: "X", value: "x" },
+  { title: "L", value: "l" },
+  { title: "XL", value: "xl" },
+];
+
+const tShirt = {
+  name: "tShirt",
+  title: "TShirt",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) =>
+        Rule.required().max(50).error("Maximum 50 Characters"),
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "name",
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+      validation: (Rule) =>
+        Rule.required().min(100).error("Minimum 100 Characters"),
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+      validation: (Rule) =>
+        Rule.required().min(100).error("Minimum 100 Characters"),
+    }),
+    defineField({
+      name: "discount",
+      title: "Discount",
+      type: "number",
+      initialValue: 0,
+      validation: (Rule) => Rule.min(0),
+    }),
+    defineField({
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            { name: "url", type: "url", title: "URL" },
+            { name: "file", type: "file", title: "File" },
+          ],
+        },
+      ],
+      validation: (Rule) =>
+        Rule.required().min(3).error("Minimum of 3 images required"),
+    }),
+    defineField({
+      name: "coverImage",
+      title: "Cover Image",
+      type: "object",
+      fields: [
+        { name: "url", type: "url", title: "URL" },
+        { name: "file", type: "file", title: "File" },
+      ],
+      validation: (Rule) => Rule.required().error("Cover Image is required"),
+    }),
+    defineField({
+      name: "size",
+      title: "Shirt Size",
+      type: "string",
+      options: {
+        list: shirtSize,
+      },
+      validation: (Rule) => Rule.required(),
+      initialValue: "x",
+    }),
+    defineField({
+      name: "specialNote",
+      title: "Special Note",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+      initialValue: "Get Premium Product",
+    }),
+
+    defineField({
+      name: "isBooked",
+      title: "Is Booked",
+      type: "boolean",
+      initialValue: false,
+    }),
+    defineField({
+      name: "isFeatured",
+      title: "Is Featured",
+      type: "boolean",
+      initialValue: false,
+    }),
+    defineField({
+      name: "reviews",
+      title: "Reviews",
+      type: "array",
+      of: [{ type: "review" }],
+    }),
+  ],
+};
+
+export default tShirt;
