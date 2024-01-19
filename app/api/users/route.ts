5e3521bad319b770c19448fe8939c438e41fd7c2
Asish Bhandari <125468428+cynic-ace@users.noreply.github.com>
2024-01-19 16:00:34 +0545

    almost done

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 
 import { authOptions } from "@/lib/auth";
 import {
-  // checkReviewExists,
-  // createReview,
+  checkReviewExists,
+  createReview,
   getUserData,
-  // updateReview,
+  updateReview,
 } from "@/lib/apis";
 
 export async function GET(req: Request, res: Response) {
@@ -25,44 +25,44 @@ export async function GET(req: Request, res: Response) {
   }
 }
 
-// export async function POST(req: Request, res: Response) {
-//   const session = await getServerSession(authOptions);
+export async function POST(req: Request, res: Response) {
+  const session = await getServerSession(authOptions);
 
-//   if (!session) {
-//     return new NextResponse("Authentication Required", { status: 500 });
-//   }
+  if (!session) {
+    return new NextResponse("Authentication Required", { status: 500 });
+  }
 
-//   const { tshirtId, reviewText, ratingValue } = await req.json();
+  const { tshirtId, reviewText, ratingValue } = await req.json();
 
-//   if (!tshirtId || !reviewText || !ratingValue) {
-//     return new NextResponse("All fields are required", { status: 400 });
-//   }
+  if (!tshirtId || !reviewText || !ratingValue) {
+    return new NextResponse("All fields are required", { status: 400 });
+  }
 
-//   const userId = session.user.id;
+  const userId = session.user.id;
 
-//   try {
-//     const alreadyExists = await checkReviewExists(userId, tshirtId);
+  try {
+    const alreadyExists = await checkReviewExists(userId, tshirtId);
 
-//     let data;
+    let data;
 
-//     if (alreadyExists) {
-//       data = await updateReview({
-//         reviewId: alreadyExists._id,
-//         reviewText,
-//         userRating: ratingValue,
-//       });
-//     } else {
-//       data = await createReview({
-//         tShirtId: tshirtId,
-//         reviewText,
-//         userId,
-//         userRating: ratingValue,
-//       });
-//     }
+    if (alreadyExists) {
+      data = await updateReview({
+        reviewId: alreadyExists._id,
+        reviewText,
+        userRating: ratingValue,
+      });
+    } else {
+      data = await createReview({
+        tShirtId: tshirtId,
+        reviewText,
+        userId,
+        userRating: ratingValue,
+      });
+    }
 
-//     return NextResponse.json(data, { status: 200, statusText: "Successful" });
-//   } catch (error: any) {
-//     console.log("Error Updating", error);
-//     return new NextResponse("Unable to create review", { status: 400 });
-//   }
-// }
+    return NextResponse.json(data, { status: 200, statusText: "Successful" });
+  } catch (error: any) {
+    console.log("Error Updating", error);
+    return new NextResponse("Unable to create review", { status: 400 });
+  }
+}
